28469960d5098beccc2f31ff37fd89798f0c2944
Igor Follador <igorledf@hotmail.com>
2022-04-25 23:44:07 -0300

    Creating basic layout and opening image A and image B

---
+++ furi-imageProcessing/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace furi_imageProcessing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pbA = new System.Windows.Forms.PictureBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.pbB = new System.Windows.Forms.PictureBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.pbResult = new System.Windows.Forms.PictureBox();
+            this.btnA = new System.Windows.Forms.Button();
+            this.btnB = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbA)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbB)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbResult)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pbA);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(250, 275);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Image A";
+            // 
+            // pbA
+            // 
+            this.pbA.Location = new System.Drawing.Point(16, 21);
+            this.pbA.Name = "pbA";
+            this.pbA.Size = new System.Drawing.Size(215, 240);
+            this.pbA.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbA.TabIndex = 0;
+            this.pbA.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.pbB);
+            this.groupBox2.Location = new System.Drawing.Point(317, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(250, 275);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Image B";
+            // 
+            // pbB
+            // 
+            this.pbB.Location = new System.Drawing.Point(16, 21);
+            this.pbB.Name = "pbB";
+            this.pbB.Size = new System.Drawing.Size(215, 240);
+            this.pbB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbB.TabIndex = 0;
+            this.pbB.TabStop = false;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.pbResult);
+            this.groupBox3.Location = new System.Drawing.Point(611, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(250, 275);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Result";
+            // 
+            // pbResult
+            // 
+            this.pbResult.Location = new System.Drawing.Point(16, 21);
+            this.pbResult.Name = "pbResult";
+            this.pbResult.Size = new System.Drawing.Size(215, 240);
+            this.pbResult.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbResult.TabIndex = 0;
+            this.pbResult.TabStop = false;
+            // 
+            // btnA
+            // 
+            this.btnA.Location = new System.Drawing.Point(64, 293);
+            this.btnA.Name = "btnA";
+            this.btnA.Size = new System.Drawing.Size(125, 30);
+            this.btnA.TabIndex = 3;
+            this.btnA.Text = "Load Image A";
+            this.btnA.UseVisualStyleBackColor = true;
+            this.btnA.Click += new System.EventHandler(this.btnA_Click);
+            // 
+            // btnB
+            // 
+            this.btnB.Location = new System.Drawing.Point(374, 293);
+            this.btnB.Name = "btnB";
+            this.btnB.Size = new System.Drawing.Size(125, 30);
+            this.btnB.TabIndex = 4;
+            this.btnB.Text = "Load Image B";
+            this.btnB.UseVisualStyleBackColor = true;
+            this.btnB.Click += new System.EventHandler(this.btnB_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(881, 450);
+            this.Controls.Add(this.btnB);
+            this.Controls.Add(this.btnA);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbA)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbB)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbResult)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pbA;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.PictureBox pbB;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.PictureBox pbResult;
+        private System.Windows.Forms.Button btnA;
+        private System.Windows.Forms.Button btnB;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ furi-imageProcessing/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace furi_imageProcessing
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap img1;
+        private byte[,] vImg1Gray;
+        private byte[,] vImg1R;
+        private byte[,] vImg1G;
+        private byte[,] vImg1B;
+        private byte[,] vImg1A;
+
+
+        private Bitmap img2;
+        private byte[,] vImg2Gray;
+        private byte[,] vImg2R;
+        private byte[,] vImg2G;
+        private byte[,] vImg2B;
+        private byte[,] vImg2A;
+
+
+        private Bitmap imgR;
+        private byte[,] vImg3Gray;
+        private byte[,] vImg3R;
+        private byte[,] vImg3G;
+        private byte[,] vImg3B;
+        private byte[,] vImg3A;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnA_Click(object sender, EventArgs e)
+        {
+            // Abre a caixa de diálogo Open
+            var filePath = string.Empty;
+            openFileDialog1.InitialDirectory = "C:\\MatLab"; // Define o diretorio padrâo de selcao de imagem
+            openFileDialog1.Filter = "TIFF image (*.tif)|*.tif" + // Define as possiveis extensoes das imagens
+                "|JPG image (*.jpg)|*.jpg" +
+                "|BMP image (*.bmp)|*.bmp" +
+                "|PNG image (*.png)|*.png" +
+                "|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            // Se o arquivo foi localizado com sucesso...
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                filePath = openFileDialog1.FileName;
+
+                bool bLoadImgOk = false;
+
+                try
+                {
+                    img1 = new Bitmap(filePath);
+                    bLoadImgOk = true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message,
+                        "Error opening image A",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+                    bLoadImgOk = false;
+                }
+
+                if (bLoadImgOk)
+                {
+                    // Adiciona 
+                    pbA.Image = img1;
+                    vImg1Gray = new byte[img1.Width, img1.Height];
+                    vImg1R = new byte[img1.Width, img1.Height];
+                    vImg1G = new byte[img1.Width, img1.Height];
+                    vImg1B = new byte[img1.Width, img1.Height];
+                    vImg1A = new byte[img1.Width, img1.Height];
+
+                }
+            }
+        }//Fim do evento click btnA
+
+        private void btnB_Click(object sender, EventArgs e)
+        {
+            // Abre a caixa de diálogo Open
+            var filePath = string.Empty;
+            openFileDialog1.InitialDirectory = "C:\\MatLab"; //Define o diretorio padrâo de selcao de imagem
+            openFileDialog1.Filter = "TIFF image (*.tif)|*.tif" + //Define as possiveis extensoes das imagens
+                "|JPG image (*.jpg)|*.jpg" +
+                "|BMP image (*.bmp)|*.bmp" +
+                "|PNG image (*.png)|*.png" +
+                "|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            //Se o arquivo foi localizado com sucesso...
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                filePath = openFileDialog1.FileName;
+
+                bool bLoadImgOk = false;
+
+                try
+                {
+                    img2 = new Bitmap(filePath);
+                    bLoadImgOk = true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message,
+                        "Error opening image B",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+                    bLoadImgOk = false;
+                }
+
+                if (bLoadImgOk)
+                {
+                    pbB.Image = img2;
+                }
+            }
+        }//Fim do evento click btB
+    }
+}
